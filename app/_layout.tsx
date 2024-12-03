825674f231e8a5d1050e7cb50f27e34bb3461b0f
Jonathan Ramirez Hdz <jonnyrmzhdz@gmail.com>
2024-12-03 06:10:02 -0600

    Creando primeros componentes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index"/>
+      <Stack.Screen name="TruckSelection" options={{title: 'Truck'}}/>
+      <Stack.Screen name="MapView" options={{title: 'Map'}}/>
+    </Stack>
+  );
 }
